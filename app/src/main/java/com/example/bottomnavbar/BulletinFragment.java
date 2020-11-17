9a7511b1431fa231d7aacf9a2b1437959d44f059
Darren Loh <vanhfdarren@gmail.com>
2020-11-17 13:32:10 +0800

    Added bulletin recyclerview into fragment and its functionalities

--- app/src/main/java/com/example/bottomnavbar/BulletinFragment.java
+++ app/src/main/java/com/example/bottomnavbar/BulletinFragment.java
@@ -1,12 +1,23 @@
 package com.example.bottomnavbar;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,6 +57,17 @@ public class BulletinFragment extends Fragment {
         return fragment;
     }
 
+    //-------initialize----------
+    private RecyclerView bulletinBoardRecView;
+    private FloatingActionButton addPost;
+    //private Button addPost;
+    private ArrayList<BulletinBoardPost> bulletinBoardPosts;
+    private BulletinBoardPostRecViewAdapter adapter;
+    // for popup
+    private int activity_code = 1;
+    //-----------
+    //---------------------------
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +81,62 @@ public class BulletinFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bulletin, container, false);
+        View view = inflater.inflate(R.layout.fragment_bulletin, container, false);
+        bulletinBoardRecView = view.findViewById(R.id.bulletin_board_post_RecView);
+
+        bulletinBoardPosts = new ArrayList<>();
+
+        adapter = new BulletinBoardPostRecViewAdapter(getContext());
+        adapter.setPosts(bulletinBoardPosts);
+        bulletinBoardRecView.setAdapter(adapter);
+
+        bulletinBoardRecView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        addPost = view.findViewById(R.id.btn_add_post);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //bulletinBoardPosts.add(new BulletinBoardPost("title", "yoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyo"));
+
+                startActivityForResult(new Intent(getContext(),BulletinPopUp.class),activity_code);
+
+                //adapter.setPosts(bulletinBoardPosts);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == activity_code){
+            if(resultCode == Activity.RESULT_OK){
+                String txtTitle = data.getStringExtra("txtTitle");
+                String txtDescription = data.getStringExtra("txtDescription");
+//                textView.setText(str1);
+//                textView2.setText(str2);
+                if(txtTitle.length() == 0 && txtDescription.length() == 0){
+                    Toast.makeText(getContext(),"Please fill in the Title and Description", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(txtTitle.length() == 0){
+                    Toast.makeText(getContext(),"Please fill in the Title", Toast.LENGTH_SHORT).show();
+                }
+                else if(txtDescription.length() == 0){
+                    Toast.makeText(getContext(),"Please fill in the Description", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    bulletinBoardPosts.add(new BulletinBoardPost(txtTitle, txtDescription));
+                    adapter.setPosts(bulletinBoardPosts);
+                }
+
+
+            }
+        }
     }
 }
