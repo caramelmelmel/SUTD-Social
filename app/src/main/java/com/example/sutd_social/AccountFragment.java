3cd57d513178e68a3b0c81164ddaf1c61cb883fb
Sorahawk <lxskyhawk98@gmail.com>
2020-12-02 04:25:14 +0800

    account fragment

--- app/src/main/java/com/example/sutd_social/AccountFragment.java
+++ app/src/main/java/com/example/sutd_social/AccountFragment.java
@@ -1,16 +1,18 @@
 package com.example.sutd_social;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.text.Layout;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,8 +21,12 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.sutd_social.R;
 import com.example.sutd_social.firebase.Admin;
 import com.example.sutd_social.firebase.Social;
+import com.example.sutd_social.firebase.User;
 import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -70,75 +76,110 @@ public class AccountFragment extends Fragment {
         }
     }
     //init views-------
-    private EditText editText_skills, editText_pillar;
-    private Button btn_save;
+    private ImageView displayPic;
+    private Button saveButton;
+    private EditText username, bio, inputSkills, fifthRows, teleContact;
+    private ChipGroup skillGroup, pillarGroup;
     //---------
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,  @Nullable ViewGroup container,
-                              @Nullable Bundle savedInstanceState) {
+                             @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account, container, false);
-//        editText_pillar = view.findViewById(R.id.edit_pillar);  // chips?
-//        editText_skills = view.findViewById(R.id.add_skills); // skill edit text
-//        btn_save = view.findViewById(R.id.save_profile);
 
-        // Show DisplayPic
-        String url = Social.getDisplayPic(Admin.getUserid());
-        Social.displayImage(getActivity(), url, view.findViewById(R.id.profile_picture));
-
-        // Get image from user
-        ImageButton profile_pic = view.findViewById(R.id.profile_picture);
-        profile_pic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(openGallery, 10);
-            }
-        });
-
+        displayPic = view.findViewById(R.id.profile_picture);
+        username = view.findViewById(R.id.username);
+        bio = view.findViewById(R.id.edit_bio);
+        inputSkills = view.findViewById(R.id.add_skills);
+        fifthRows = view.findViewById(R.id.edit_fifth_rows);
+        teleContact = view.findViewById(R.id.edit_contact);
+        pillarGroup = view.findViewById(R.id.edit_pillar);
+        skillGroup = view.findViewById(R.id.skills_display);
+        saveButton = view.findViewById(R.id.save_profile);
         return view;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode==10) {
-            if (resultCode== Activity.RESULT_OK) {
-                Uri imageUri = data.getData();
-                Social.addImage(Admin.getUserid(), imageUri);
-            }
-        }
-    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        btn_save.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //String student_skill = editText_skills.getText().toString();
-//                //String student_pillar = editText_pillar.getText().toString();
-////                if (!student_pillar.isEmpty()){
-////                    Social.setAttr("Pillar", Admin.getUserid(), student_pillar);
-////                }
-////                if (!student_skill.isEmpty()){
-////                    Social.setAttr("Skills", Admin.getUserid(), student_skill);
-////                }
-//            }
-//        });
-    }
 
-//    private MainViewModel mViewModel;
-//    public static AccountFragment newInstance() {
-//        return new AccountFragment();
-//    }
-//
-//    @Override
-//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-//
-//        Chip skill1 = getView().findViewById(R.id.skill_chip1);
-//    }
+        List<Integer> chipIds = skillGroup.getCheckedChipIds();
+        for (Integer chipId: chipIds) {
+            Chip skillChip = skillGroup.findViewById(chipId);
+            skillChip.setOnCloseIconClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    skillGroup.removeView(skillChip);
+                }
+            });
+        }
+
+        inputSkills.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                Chip newChip = (Chip) inflater.inflate(R.layout.skill_chip, null);
+                newChip.setText(inputSkills.getText());
+                newChip.setCheckedIcon(null);
+                skillGroup.addView(newChip);
+                inputSkills.setText("");
+
+                newChip.setOnCloseIconClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        skillGroup.removeView(newChip);
+                    }
+                });
+
+                return false;
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user_name = username.getText().toString();
+                if (user_name != null) {
+                    Social.setAttr("Name", Admin.getUserid(), user_name);
+                }
+
+                String info = bio.getText().toString();
+                if (info != null) {
+                    Social.setAttr("Info", Admin.getUserid(), info);
+                }
+
+                String fifth_row = fifthRows.getText().toString();
+                if (fifth_row != null) {
+                    Social.setAttr("FifthRow", Admin.getUserid(), fifth_row);
+                }
+
+                String telegram = teleContact.getText().toString();
+                if (telegram != null) {
+                    Social.setAttr("Telegram", Admin.getUserid(), telegram);
+                }
+
+                Integer checkedId = pillarGroup.getCheckedChipId();
+                if (checkedId != -1) {
+                    Chip pillarChip = pillarGroup.findViewById(checkedId);
+                    String studentPillar = pillarChip.getText().toString();
+                    Social.setAttr("Pillar", Admin.getUserid(), studentPillar);
+                }
+
+                HashMap<String, Long> skills = new HashMap<>();
+                List<Integer> chipIds = skillGroup.getCheckedChipIds();
+                for (Integer chipId: chipIds) {
+                    Chip skillChip = skillGroup.findViewById(chipId);
+                    skills.put(skillChip.getText().toString(), new Long(50));
+                }
+
+                if (!skills.isEmpty()) {
+                    Social.setAttr("Skills", Admin.getUserid(), skills);
+                }
+
+
+            }
+        });
+    }
 }
